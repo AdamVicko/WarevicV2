59e946a7874adfcfb65aea600bd0d32ddbdb922d
AdamVicko <vickovic2203@gmail.com>
2023-05-15 22:10:28 +0200

    feat: adding methods in oxygenConcentrator model
    Adding al CRUD methods.

--- app/model/oxygenConcentrator.php
+++ app/model/oxygenConcentrator.php
@@ -2,45 +2,152 @@
 
 class OxygenConcentrator
 {
-    public static function read($uvjet='',$stranica=1)
+    public static function read($condition='',$page=1)
     {
 
-        $uvjet = '%' . $uvjet . '%';
-        $brps = App::config('brps');
-        $pocetak = ($stranica * $brps) - $brps;
+        $condition = '%' . $condition . '%';
+        $nrpp = App::config('nrpp');
+        $start = ($page * $nrpp) - $nrpp;
 
-        $veza = DB::getInstance(); //read napravljen da nemogu brisati OC ako nije prikupljen
-        $izraz = $veza->prepare('
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
         
-        SELECT a.sifra,
+        SELECT a.id,
                 a.serialNumber,
                 a.workingHour,
                 a.manufacturer,
                 a.model,
                 a.oxygenConcentratorComment,
                 a.buyingDate,
-                c.active AS delivered
+                b.active AS delivered
         FROM oxygenConcentrator a
-            LEFT JOIN collection b ON a.sifra = b.oxygenConcentrator
-            LEFT JOIN delivery c ON a.sifra = c.oxygenConcentrator
+            LEFT JOIN delivery b ON a.id = b.oxygenConcentrator
+            LEFT JOIN collection c ON b.collection = c.delivery
         WHERE a.serialNumber
-        LIKE :uvjet
-        GROUP BY a.sifra,
+        LIKE :condition
+        GROUP BY a.id,
                 a.serialNumber,
                 a.workingHour,
                 a.manufacturer,
                 a.model,
                 a.oxygenConcentratorComment,
                 a.buyingDate,
-                c.active 
-        ORDER BY a.buyingDate ASC LIMIT :pocetak, :brps;
+                b.active 
+        ORDER BY a.buyingDate ASC LIMIT :start, :nrpp;
 
         ');
-        $izraz->bindValue('pocetak',$pocetak, PDO::PARAM_INT); // param int tako da mi salje int a ne string
-        $izraz->bindValue('brps',$brps, PDO::PARAM_INT); // param int tako da mi salje int a ne string
-        $izraz->bindParam('uvjet', $uvjet);
+        $expression->bindValue('start',$start, PDO::PARAM_INT); 
+        $expression->bindValue('nrpp',$nrpp, PDO::PARAM_INT); 
+        $expression->bindParam('condition', $condition);
 
-        $izraz->execute();
-        return $izraz->fetchAll();
+        $expression->execute();
+        return $expression->fetchAll();
     }
+
+    public static function allOxygen($condition='')
+    {
+        $condition = '%' . $condition . '%';
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
+        
+        SELECT COUNT(*)
+        FROM 
+            oxygenConcentrator
+        WHERE serialNumber
+        LIKE :condition;
+        
+        ');
+        $expression->execute([
+            'condition'=>$condition
+        ]);
+        return $expression->fetchColumn();
+    }
+
+    public static function create($parameters)
+    {
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
+        
+        INSERT INTO oxygenConcentrator (serialNumber,workingHour,manufacturer,model,oxygenConcentratorComment,buyingDate)
+        VALUES(:serialNumber,:workingHour,:manufacturer,:model,:oxygenConcentratorComment,:buyingDate);
+
+        ');
+        $expression->execute($parameters);
+    }
+
+    public static function update($parameters)
+    {
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
+        
+        UPDATE oxygenConcentrator  SET
+            serialNumber=:serialNumber,
+            workingHour=:workingHour,
+            manufacturer=:manufacturer,
+            model=:model,
+            oxygenConcentratorComment=:oxygenConcentratorComment,
+            buyingDate=:buyingDate 
+        WHERE id=:id
+
+
+        ');
+        $expression->execute($parameters);
+    }
+
+    public static function delete($id)
+    {
+        $connection = DB::getInstance();
+
+        $expression = $connection->prepare('
+        
+        DELETE FROM delivery
+        WHERE oxygenConcentrator=:id
+    
+        ');
+        $expression->execute([
+            'id'=>$id
+        ]);
+
+        $expression = $connection->prepare('
+        
+        DELETE FROM oxygenConcentrator
+        WHERE id=:id
+    
+        ');
+        $expression->execute([
+            'id'=>$id
+        ]);
+
+    }
+
+    public static function firstOxygenConcentrator()
+    {
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
+        
+        SELECT id FROM oxygenConcentrator
+        ORDER BY id LIMIT 1;
+
+        ');
+        $expression->execute();
+        $id=$expression->fetchColumn();
+        return (int)$id;
+    }
+    
+    public static function readOne($id)
+    {
+        
+        $connection = DB::getInstance();
+        $expression = $connection->prepare('
+        
+            SELECT * FROM oxygenConcentrator
+            WHERE id=:id
+        
+        ');
+        $expression->execute([
+            'id'=>$id
+        ]);
+        return $expression->fetch();
+    }
+
 }
